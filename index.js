388b1db63a1a6afd09131999662348487ce4edd6
Sourit Sarkar <sourit.sarkar@gmail.com>
2022-10-23 19:02:56 +0530

    almost done!

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8000;
 
 // use express router
-app.use('/', require('./routes'));
+// app.use('/', require('./routes'));
 
 //set up view engine
 app.set('view engine', 'ejs');
-app.set('views', './views');
+// app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.urlencoded());
 
-var taskList = [
-    {
-        name: "Cooking",
-        date: "23/09/2011",
-        category: "Cleaning"
-    }
-];
+var taskList = [];
 
 app.get('/', function(req, res){
     return res.render('home',{
@@ -24,9 +23,38 @@ app.get('/', function(req, res){
     });
 });
 
+app.post('/add-task', function(req, res){
+    taskList.push(req.body);
+    return res.redirect('/');
+});
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error in running the server ${err}`);
     }
     console.log(`Server is running on port: ${port}`);
 });
+
+// app.get('/remove-task/', function(req, res){
+//     console.log(req.query);
+//     let name = req.query.name;
+
+//     let taskindex = taskList.findIndex(task => taskList.name == name);
+
+//     if(taskindex != -1){
+//         taskList.splice(taskindex, 1);
+//     }
+
+//     return res.redirect('back');
+// });
+
+app.post("/remove-task/", function(req, res) {
+    const checkedItemId = req.body.checkbox;
+    console.log(checkedItemId);
+    taskList.findByIdAndRemove(checkedItemId , function(err) {
+      if (!err) {
+        console.log("Successfully deleted the Id");
+      }
+      res.redirect("/");
+    });
+  });
